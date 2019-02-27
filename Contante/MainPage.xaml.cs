b5a18520db2fc4ed34ea1ec1dcfde976d4540baf
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-02-26 20:13:08 -0400

    tela de contagem de cliques

---
+++ Contante/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contante
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ClicadaDoBotao(object sender, EventArgs e){
+
+            label_status.Text = "Voce clicou " + i + " Vezes";
+
+            button_clique.Text = "Novo Botao";
+
+            i++;
+        }
+    }
+}
